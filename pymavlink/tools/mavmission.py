2792d57813571c6403c565375578873d22a44f1b
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-10-12 12:59:24 +0800

    Rebase from 'debian/jazzy/mavlink'

--- pymavlink/tools/mavmission.py
+++ pymavlink/tools/mavmission.py
@@ -22,19 +22,42 @@ def mavmission(logfile):
 
     wp = mavwp.MAVWPLoader()
 
+    num_wps = None
     while True:
-        m = mlog.recv_match(type=['MISSION_ITEM','CMD','WAYPOINT'])
+        m = mlog.recv_match(type=['MISSION_ITEM','CMD','WAYPOINT','MISSION_ITEM_INT'])
         if m is None:
             break
         if m.get_type() == 'CMD':
+            try:
+                frame = m.Frame
+            except AttributeError:
+                print("Warning: assuming frame is GLOBAL_RELATIVE_ALT")
+                frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+            num_wps = m.CTot
             m = mavutil.mavlink.MAVLink_mission_item_message(0,
                                                              0,
                                                              m.CNum,
-                                                             mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                                                             frame,
                                                              m.CId,
                                                              0, 1,
                                                              m.Prm1, m.Prm2, m.Prm3, m.Prm4,
                                                              m.Lat, m.Lng, m.Alt)
+
+        if m.get_type() == 'MISSION_ITEM_INT':
+            m = mavutil.mavlink.MAVLink_mission_item_message(m.target_system,
+                                                             m.target_component,
+                                                             m.seq,
+                                                             m.frame,
+                                                             m.command,
+                                                             m.current,
+                                                             m.autocontinue,
+                                                             m.param1,
+                                                             m.param2,
+                                                             m.param3,
+                                                             m.param4,
+                                                             m.x*1.0e-7,
+                                                             m.y*1.0e-7,
+                                                             m.z)
         if m.current >= 2:
             continue
 
@@ -42,6 +65,11 @@ def mavmission(logfile):
             print("Adding dummy WP %u" % wp.count())
             wp.set(m, wp.count())
         wp.set(m, m.seq)
+
+    if num_wps is not None:
+        while wp.count() > num_wps:
+            wp.remove(wp.wp(wp.count() - 1))
+
     wp.save(args.output)
     print("Saved %u waypoints to %s" % (wp.count(), args.output))
 
